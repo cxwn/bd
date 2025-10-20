c4e3f584902aca3dfb344447f72fa36be2cbdae3
IVAN DU <mrivandu@hotmail.com>
2025-10-20 18:35:13 +0800

    fix:修正一些错误

--- logstash/docker/docker-entrypoint.sh
+++ logstash/docker/docker-entrypoint.sh
@@ -4,7 +4,7 @@
 # Note that this will mutate logstash.yml in place if any such settings are found.
 # This may be undesirable, especially if logstash.yml is bind-mounted from the
 # host system.
-env2yaml /usr/share/logstash/config/logstash.yml
+env2yaml /usr/local/logstash/config/logstash.yml
 
 if [[ -n "$LOG_STYLE" ]]; then
   case "$LOG_STYLE" in
@@ -14,7 +14,7 @@ if [[ -n "$LOG_STYLE" ]]; then
     file)
       # Overwrite the default config with the stack config. Do this as a
       # copy, not a move, in case the container is restarted.
-      cp -f /usr/share/logstash/config/log4j2.file.properties /usr/share/logstash/config/log4j2.properties
+      cp -f /usr/local/logstash/config/log4j2.file.properties /usr/local/logstash/config/log4j2.properties
       ;;
     *)
       echo "ERROR: LOG_STYLE set to [$LOG_STYLE]. Expected [console] or [file]" >&2
